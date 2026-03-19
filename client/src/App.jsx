b1a740595e431b57ba98a46b17e52e4763714a04
Pasindu Subasinghe <phimansa@gmail.com>
2026-03-20 03:16:55 +0530

    Recent updates commited

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import ProfilePage from "./pages/ProfilePage";
 import { Toaster } from "react-hot-toast";
 import { useContext } from "react";
 import { AuthContext } from "../context/authContext";
-import assets from "./assets/assets";
+import assets from "./assets/assets.js";
 
 const App = () => {
   const { authUser } = useContext(AuthContext);
